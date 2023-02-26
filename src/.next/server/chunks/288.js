8b41fa20b8bdbb37f3c35580512c5d543c348580
GiovannyLeone <giovanny.leone.barbosa@gmail.com>
2023-02-26 19:52:44 -0300

    Add the next folder

---
+++ src/.next/server/chunks/288.js
@@ -0,0 +1,339 @@
+exports.id = 288;
+exports.ids = [288];
+exports.modules = {
+
+/***/ 6553:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"footer": "Footer_footer__Dhw_9",
+	"listFooter": "Footer_listFooter__hmlM9",
+	"footerPowerprime": "Footer_footerPowerprime__GwbrC",
+	"listMapSite": "Footer_listMapSite__OnWvs",
+	"divNetwork": "Footer_divNetwork__D2xTJ",
+	"footerNetwork": "Footer_footerNetwork__cxR1m",
+	"listIcons": "Footer_listIcons__2DJTW",
+	"iconNetwork": "Footer_iconNetwork__GrdsI",
+	"infoFooter": "Footer_infoFooter__mky56"
+};
+
+
+/***/ }),
+
+/***/ 725:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"header": "Home_header__hLfb_",
+	"contentHeader": "Home_contentHeader__ruVaq",
+	"divLogo": "Home_divLogo__grR6Y",
+	"logoPowerPrime": "Home_logoPowerPrime__bounu",
+	"navHeader": "Home_navHeader__sOfLb"
+};
+
+
+/***/ }),
+
+/***/ 3459:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({"src":"/_next/static/media/poweprime-black.a6de90f6.png","height":600,"width":600,"blurDataURL":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAYAAADED76LAAAA1UlEQVR42mOAgXmp2owMQDApyUPg7jQGq1UNDAoMIMDCwsLIxsrK2O0vxswABJUMDDbP5oTfubCxdjcDDqA5fWLLkdKqwisMDAwKIBOEgZiHhZVVLlyLgZWBIZwjw5YhuNafYbOZsWEKSME2IO5kZWFp5GZnXc7AwDDLWZt/doqjwCIhZoYOkIJIIC5lZWWdysTCkiXIxRrBwMCaCMTzGJjZo0EKvIF4KRBvACrqZ2ZhWczFzrpYgJN1Ow87qzhIgRgQ8wCxMhBrA7EWEGsCsQULC4sgABQgJrdTesm2AAAAAElFTkSuQmCC","blurWidth":8,"blurHeight":8});
+
+/***/ }),
+
+/***/ 3080:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(9894);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _Footer_module_scss__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(6553);
+/* harmony import */ var _Footer_module_scss__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_Footer_module_scss__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+const Footer = ()=>{
+    const blank = "_blank";
+    const linkAboutUs = "/#AboutUs_aboutUs__5odSJ";
+    const linkLoja = "https://chipparacartuchos.com/";
+    const linkContact = "/contato";
+    const linkInstagram = "https://www.instagram.com/powerprime.br/";
+    const linkFacebook = "https://www.facebook.com/powerprime.com.br";
+    const linkWhatsApp = "https://api.whatsapp.com/send?phone=551138724433&text=Ol%C3%A1%2C%20vim%20pelo%20site%20da%20Power%20Prime%20quero%20conhecer%20seus%20produtos";
+    const linkYoutube = "https://www.youtube.com/channel/UC_c0q28LHg_0SOdeVrmrPmg";
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+        children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+            id: (_Footer_module_scss__WEBPACK_IMPORTED_MODULE_2___default().footer),
+            children: [
+                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                    id: (_Footer_module_scss__WEBPACK_IMPORTED_MODULE_2___default().listFooter),
+                    children: [
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                            className: (_Footer_module_scss__WEBPACK_IMPORTED_MODULE_2___default().footerPowerprime),
+                            children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("ul", {
+                                className: (_Footer_module_scss__WEBPACK_IMPORTED_MODULE_2___default().listMapSite),
+                                children: [
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                                            children: "Power Prime"
+                                        })
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_1___default()), {
+                                            href: linkAboutUs,
+                                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                                children: "Sobre N\xf3s"
+                                            })
+                                        })
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_1___default()), {
+                                            href: linkContact,
+                                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                                children: "Contato"
+                                            })
+                                        })
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_1___default()), {
+                                            href: linkLoja,
+                                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                                target: blank,
+                                                children: "Loja"
+                                            })
+                                        })
+                                    })
+                                ]
+                            })
+                        }),
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                            className: (_Footer_module_scss__WEBPACK_IMPORTED_MODULE_2___default().footerNetwork),
+                            children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("ul", {
+                                className: (_Footer_module_scss__WEBPACK_IMPORTED_MODULE_2___default().listIcons),
+                                children: [
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                                            children: "Siga-nos"
+                                        })
+                                    }),
+                                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                        className: (_Footer_module_scss__WEBPACK_IMPORTED_MODULE_2___default().divNetwork),
+                                        children: [
+                                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                                                children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_1___default()), {
+                                                    href: linkInstagram,
+                                                    children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                                        target: blank,
+                                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                                            className: (_Footer_module_scss__WEBPACK_IMPORTED_MODULE_2___default().iconNetwork)
+                                                        })
+                                                    })
+                                                })
+                                            }),
+                                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                                                children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_1___default()), {
+                                                    href: linkFacebook,
+                                                    children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                                        target: blank,
+                                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                                            className: (_Footer_module_scss__WEBPACK_IMPORTED_MODULE_2___default().iconNetwork)
+                                                        })
+                                                    })
+                                                })
+                                            }),
+                                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                                                children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_1___default()), {
+                                                    href: linkWhatsApp,
+                                                    children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                                        target: blank,
+                                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                                            className: (_Footer_module_scss__WEBPACK_IMPORTED_MODULE_2___default().iconNetwork)
+                                                        })
+                                                    })
+                                                })
+                                            }),
+                                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                                                children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_1___default()), {
+                                                    href: linkYoutube,
+                                                    children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                                        target: blank,
+                                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                                            className: (_Footer_module_scss__WEBPACK_IMPORTED_MODULE_2___default().iconNetwork)
+                                                        })
+                                                    })
+                                                })
+                                            })
+                                        ]
+                                    })
+                                ]
+                            })
+                        })
+                    ]
+                }),
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                    id: (_Footer_module_scss__WEBPACK_IMPORTED_MODULE_2___default().infoFooter),
+                    children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                        children: "\xa9 2022 Power Prime. Todos os direitos reservados"
+                    })
+                })
+            ]
+        })
+    });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Footer);
+
+
+/***/ }),
+
+/***/ 6045:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "Z": () => (/* binding */ Header_Header)
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+// EXTERNAL MODULE: external "next/head"
+var head_ = __webpack_require__(968);
+var head_default = /*#__PURE__*/__webpack_require__.n(head_);
+// EXTERNAL MODULE: ../node_modules/next/link.js
+var next_link = __webpack_require__(9894);
+var link_default = /*#__PURE__*/__webpack_require__.n(next_link);
+// EXTERNAL MODULE: ../node_modules/next/image.js
+var next_image = __webpack_require__(8579);
+var image_default = /*#__PURE__*/__webpack_require__.n(next_image);
+// EXTERNAL MODULE: ./components/Header/Home.module.scss
+var Home_module = __webpack_require__(725);
+var Home_module_default = /*#__PURE__*/__webpack_require__.n(Home_module);
+// EXTERNAL MODULE: ./public/images/header/logo/poweprime-black.png
+var poweprime_black = __webpack_require__(3459);
+;// CONCATENATED MODULE: ./public/images/header/logo/favicon-svg.svg
+/* harmony default export */ const favicon_svg = ({"src":"/_next/static/media/favicon-svg.abff0b8d.svg","height":144,"width":144});
+;// CONCATENATED MODULE: ./components/Header/Header.tsx
+
+
+
+
+
+
+
+const Header = ()=>{
+    const blank = "_blank";
+    const linkHome = "/";
+    const linkAboutUs = "/#AboutUs_aboutUs__5odSJ";
+    const linkLoja = "https://chipparacartuchos.com/";
+    const linkContact = "/contato";
+    function titlePage() {
+        if (false) {}
+    }
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)(jsx_runtime_.Fragment, {
+        children: [
+            /*#__PURE__*/ (0,jsx_runtime_.jsxs)((head_default()), {
+                children: [
+                    /*#__PURE__*/ jsx_runtime_.jsx("meta", {
+                        charSet: "UTF-8"
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("meta", {
+                        "http-equiv": "X-UA-Compatible",
+                        content: "IE=edge"
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("meta", {
+                        name: "viewport",
+                        content: "initial-scale=1.0, width=device-width"
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("link", {
+                        rel: "shortcut icon",
+                        href: favicon_svg.src,
+                        type: "image/x-icon"
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("title", {
+                        children: titlePage()
+                    })
+                ]
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                className: (Home_module_default()).header,
+                children: /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                    className: (Home_module_default()).contentHeader,
+                    children: [
+                        /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                            className: (Home_module_default()).divLogo,
+                            children: /*#__PURE__*/ jsx_runtime_.jsx((image_default()), {
+                                src: poweprime_black/* default */.Z,
+                                alt: "Logo Power Prime",
+                                width: "80px",
+                                height: "80px",
+                                className: (Home_module_default()).logoPowerPrime
+                            })
+                        }),
+                        /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                            className: (Home_module_default()).navHeader,
+                            children: /*#__PURE__*/ (0,jsx_runtime_.jsxs)("ul", {
+                                children: [
+                                    /*#__PURE__*/ jsx_runtime_.jsx("li", {
+                                        children: /*#__PURE__*/ jsx_runtime_.jsx((link_default()), {
+                                            href: linkHome,
+                                            children: /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                                                children: "Home"
+                                            })
+                                        })
+                                    }),
+                                    "|",
+                                    /*#__PURE__*/ jsx_runtime_.jsx("li", {
+                                        children: /*#__PURE__*/ jsx_runtime_.jsx((link_default()), {
+                                            href: linkLoja,
+                                            children: /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                                                target: blank,
+                                                children: "Loja"
+                                            })
+                                        })
+                                    }),
+                                    "|",
+                                    /*#__PURE__*/ jsx_runtime_.jsx("li", {
+                                        children: /*#__PURE__*/ jsx_runtime_.jsx((link_default()), {
+                                            href: linkAboutUs,
+                                            children: /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                                                children: "Sobre N\xf3s"
+                                            })
+                                        })
+                                    }),
+                                    "|",
+                                    /*#__PURE__*/ jsx_runtime_.jsx("li", {
+                                        children: /*#__PURE__*/ jsx_runtime_.jsx((link_default()), {
+                                            href: linkContact,
+                                            children: /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                                                children: "Contato"
+                                            })
+                                        })
+                                    })
+                                ]
+                            })
+                        })
+                    ]
+                })
+            })
+        ]
+    });
+};
+/* harmony default export */ const Header_Header = (Header);
+
+
+/***/ })
+
+};
+;
